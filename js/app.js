5cdac3bb88def9b2076ddbddb53b64dcb970823c
MaxWesterlund <95910612+MaxWesterlund@users.noreply.github.com>
2023-03-07 14:50:27 +0100

    Lektion 5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 document.getElementById("burger_menu").addEventListener("click", toggleLinks)
-document.getElementById("header_burger_menu").addEventListener("click", toggleLinks)
 
 function toggleLinks() {
     document.getElementById("link_list").classList.toggle("link_list_visible")
+    document.getElementById("burger_menu").classList.toggle("burger_menu_selected");
+}
+
+window.onscroll = function scroll() {
+    var blur_buffer = .05;
+    var scroll_dist = window.pageYOffset;
+    scroll_dist = Math.min(scroll_dist, 200)
+
+    console.log(scroll_dist);
+
+    document.getElementById("start_background").style.filter = "blur(" + scroll_dist * blur_buffer + "px)";;
 }
