bd92315dd99dede165f5438e690bbf3c78df9b42
MaxWesterlund <95910612+MaxWesterlund@users.noreply.github.com>
2023-03-14 10:04:10 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-document.getElementById("burger_menu").addEventListener("click", toggleLinks)
+document.getElementById("burger_menu").addEventListener("click", toggleLinks);
+document.getElementById("page_link").addEventListener("click", toggleLinks);
 
 function toggleLinks() {
-    document.getElementById("link_list").classList.toggle("link_list_visible")
+    document.getElementById("link_list").classList.toggle("link_list_closed")
     document.getElementById("burger_menu").classList.toggle("burger_menu_selected");
 }
 
